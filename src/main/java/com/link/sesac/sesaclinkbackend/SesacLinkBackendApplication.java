6c2e7233d69e3bcff18c91d656e9a57b256c4660
jang-jinuk <dlsndifn98@gmail.com>
2024-11-20 21:44:13 +0900

    first commit

---
+++ src/main/java/com/link/sesac/sesaclinkbackend/SesacLinkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.link.sesac.sesaclinkbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SesacLinkBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SesacLinkBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/link/sesac/sesaclinkbackend/SesacLinkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.link.sesac.sesaclinkbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SesacLinkBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
